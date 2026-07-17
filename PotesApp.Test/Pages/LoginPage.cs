4f4292f3ad1f91292b4972f75ec72f1a1fc84925
Darwin Potes <darwinpotes2102@live.com>
2026-07-17 12:05:19 -0500

    Add Tests

---
+++ PotesApp.Test/Pages/LoginPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+
+namespace PotesApp.Test.Pages;
+
+public class LoginPage
+{
+    private readonly IWebDriver _driver;
+
+    public LoginPage(IWebDriver driver) => _driver = driver;
+
+    // Elementos
+    private IWebElement UsernameField => _driver.FindElement(By.Id("username"));
+    private IWebElement PasswordField => _driver.FindElement(By.Id("password"));
+    private IWebElement LoginButton => _driver.FindElement(By.Id("submit"));
+
+    // Acciones
+    public void Login(string email, string password)
+    {
+        UsernameField.SendKeys(email);
+        PasswordField.SendKeys(password);
+        LoginButton.Click();
+    }
+}
---
+++ PotesApp.Test/Tests/LoginTests.cs
@@ -0,0 +1,32 @@
+﻿using PotesApp.Test.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MiProyecto.Tests.Tests;
+
+public class LoginTests
+{
+    private IWebDriver _driver;
+
+    [SetUp]
+    public void Setup()
+    {
+        _driver = new ChromeDriver();
+        _driver.Manage().Window.Maximize();
+        _driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
+    }
+
+    [Test]
+    public void TestLoginExitoso()
+    {
+        var loginPage = new LoginPage(_driver);
+        loginPage.Login("student", "Password123");
+
+        // Agregar aserciones aquí
+        Assert.Pass();
+    }
+
+    [TearDown]
+    public void Teardown() => _driver.Quit();
+}
